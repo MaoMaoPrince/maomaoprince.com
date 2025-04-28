7b264e4ffaae26deca7b8750cc0b41fbb46a664f
implicit-dog <implicit.ralph@gmail.com>
2025-04-29 00:45:01 +0100

    initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,23 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	site: 'https://maomaoprince.com',
+	integrations: [
+		starlight({
+			title: 'MaoMaoPrince.com',
+			// social: [{ icon: 'discord', label: 'MaoMaoPrince Fishing Discord', href: 'https://github.com/withastro/starlight' }],
+			
+			logo: {
+				src: './src/assets/maomaoprince_logo_256x256.svg',
+				alt: 'Logo',
+				// replacesTitle: true,
+			},
+			editLink: {
+				baseUrl: 'https://github.com/maomaoprince/maomaoprince.com/edit/main/src/',
+			},	
+		}),
+	],
+});
