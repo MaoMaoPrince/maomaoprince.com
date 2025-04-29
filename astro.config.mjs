4dd95686f7e75dedd9fc4fc12626e28a6960df63
implicit-dog <implicit.ralph@gmail.com>
2025-04-29 01:04:07 +0100

    update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://maomaoprince.github.io',
+	site: 'https://maomaoprince.github.io/maomaoprince.com/',
+	base: '/maomaoprince.com/',
 	integrations: [
 		starlight({
 			title: 'MaoMaoPrince.com',
